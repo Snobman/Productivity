5b7583fba178db5b52170a5ab7c79031c4418e30
Snobman <liufrank652@gmail.com>
2023-03-20 00:01:05 -0400

    Mentality Website

---
+++ script.js
@@ -0,0 +1,11 @@
+var startingbutton = document.getElementById("startingbutton");
+
+startingbutton.addEventListener("click", function() {
+  window.location.href = "main.html"
+  
+});
+var stressed = document.getElementById("stressed");
+
+stressed.addEventListener("click", function() {
+  alert("L bozo");
+});                         
